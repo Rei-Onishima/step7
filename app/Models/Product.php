a76531b3fab15cdee92bed5f2f79733560e00b33
Rei-Onishima <r7.milkyway.7@icloud.com>
2024-05-15 01:41:43 +0900

    Created and defined models relationships

---
+++ app/Models/Product.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Product extends Model
+{
+    use HasFactory;
+    
+    public function getList() {
+        // productsテーブルからデータを取得
+        $products = DB::table('products')->get();
+
+        return $products;
+    }
+
+    //　一括代入可能な属性
+    protected $fillable = [
+        'product_name',
+        'price',
+        'stock',
+        'company_id',
+        'comment',
+        'img_path',
+    ];
+
+    // Productモデルがsalesテーブルとリレーション関係を結ぶためのメソッド
+    public function sales()
+    {
+        return $this->hasMany(Sale::class);
+    }
+
+    // Productモデルがcompanysテーブルとリレーション関係を結ぶ為のメソッド
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+}
